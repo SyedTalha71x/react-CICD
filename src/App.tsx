c1bad3fdcf9d900da60fbecb704627f9c9dd66b7
SyedTalha71x <syedtalha71x@gmail.com>
2024-11-28 11:42:13 +0500

    some updates

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const LandingPage: React.FC = () => {
   return (
     <div style={styles.container}>
       <h1 style={styles.heading}>
-        CI/CD Automation Setup for React Application with Jenkins, Docker, Ansible & push Docker Image to Docker Hub
+        CI/CD Automation Setup for React Application with AWS Codepipeline
       </h1>
     </div>
   );
